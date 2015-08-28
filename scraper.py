6aebe2e2443fdc0e0dfb97c1f9bff01551b63251
Friedrich Lindenberg <friedrich@pudo.org>
2015-08-28 16:46:57 +0200

    begin importing the old TR scraper

--- scraper.py
+++ scraper.py
@@ -1,7 +1,9 @@
 import logging
 
+import scraper_register
 import scraper_accreditations
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+    scraper_register.extract()
     scraper_accreditations.extract()
